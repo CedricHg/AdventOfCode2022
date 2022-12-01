235d391c43a30fd17cc7702e99611608af3a690b
CedricHg <cedrichaegeman@gmail.com>
2022-12-01 23:27:55 +0100

    Add project files.

---
+++ AdventOfCode2022/1/SolutionOne.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022._1
+{
+    public static class SolutionOne
+    {
+        /// <summary>
+        /// Get the highest number
+        /// </summary>
+        public static async Task<int> AnswerPartOne()
+        {
+            var answer = (await BuildList()).OrderByDescending(x => x).First();
+            return answer;
+        }
+
+        /// <summary>
+        /// Get the sum of top 3
+        /// </summary>
+        public static async Task<int> AnswerPartTwo()
+        {
+            var answer = (await BuildList()).OrderByDescending(x => x)
+                .Select((x, i) => new { Index = i, Number = x })
+                .Where(x => x.Index < 3)
+                .Select(x => x.Number)
+                .Sum();
+
+            return answer;
+        }
+
+        /// <summary>
+        /// Read the groups of data, calculate the number per group
+        /// </summary>
+        private static async Task<List<int>> BuildList()
+        {
+            var input = await ReadInput();
+
+            List<int> counts = new List<int>();
+            int currentCount = 0;
+            foreach (var line in input)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    counts.Add(currentCount);
+                    currentCount = 0;
+                    continue;
+                }
+                else
+                {
+                    currentCount += int.Parse(line);
+                }
+            }
+
+            return counts;
+        }
+
+        private static async Task<List<string>> ReadInput()
+        {
+            return (await File.ReadAllLinesAsync("./1/input.txt")).ToList();
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2022._1;
+
+Console.WriteLine("Hello, World!");
+
+int answerPOne = await SolutionOne.AnswerPartOne();
+Console.WriteLine($"Answer to assignment 1 part 1 is {answerPOne}");
+
+int answerPTwo = await SolutionOne.AnswerPartTwo();
+Console.WriteLine($"Answer to assignment 1 part 2 is {answerPTwo}");
+var _ = Console.ReadKey();
